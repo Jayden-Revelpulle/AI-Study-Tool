54185923c46050b42362cb4a606faedce9fa181c
jayrevel2004 <jaydenrevel@gmail.com>
2025-07-22 19:20:37 -0400

    Setup basic routing

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import Modal from 'react-modal'
+import { BrowserRouter } from 'react-router-dom'
 
 Modal.setAppElement('#root')
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,45 @@
+
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Course from "../components/Course";
+import CourseModel from "../components/CourseModel";
+
+interface Course {
+  _id: string;
+  name: string;
+  color: string;
+}
+
+const API_BASE_URL = "http://localhost:3000/api/v1";
+
+export default function Home() {
+  const [courses, setCourses] = useState<Course[]>([]);
+
+  const fetchCourses = async () => {
+    try {
+      const response = await axios.get(`${API_BASE_URL}/courses`);
+      setCourses(response.data.courses);
+    } catch (err) {
+      console.error("Error fetching courses:", err);
+    }
+  };
+
+  useEffect(() => {
+    fetchCourses();
+  }, []);
+
+  return (
+    <>
+      <h1 className="text-center text-4xl mt-1">API Study Tool</h1>
+      <div className="flex flex-col items-center justify-center mt-1">
+        <h3 className="text-2xl">My Courses</h3>
+        <CourseModel onCourseCreated={fetchCourses} />
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-4">
+          {courses.map((course) => (
+            <Course key={course._id} name={course.name} color={"red"} />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
