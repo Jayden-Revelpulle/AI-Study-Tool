1e9e1358202240f90ac9c019574a54e7d9248486
jayrevel2004 <jaydenrevel@gmail.com>
2025-07-24 19:03:50 -0400

    Completed Delete course functionality and edited styles

--- client/src/components/Course.tsx
+++ client/src/components/Course.tsx
@@ -7,7 +7,11 @@ type CourseProps = {
 function Course({ name, color}: CourseProps) {
   return (
     <>
-    <div className='rounded-2xl text-center text-1.5xl p-3'style={{ backgroundColor: color }}>{name}</div>
+    <div className='rounded-2xl text-center text-amber-50 text-1.5xl p-3 transition-transform duration-200 hover:scale-105'
+    style={{ backgroundColor: color }}
+    >
+      {name}
+    </div>
     </>
   )
 }
--- client/src/pages/Course.tsx
+++ client/src/pages/Course.tsx
@@ -1,4 +1,4 @@
-import { useLocation, useParams } from "react-router-dom"
+import { useLocation, useParams, useNavigate } from "react-router-dom"
 import API_BASE_URL from "../assets/URL";
 import axios from "axios";
 
@@ -6,10 +6,12 @@ export default function Course() {
     const location = useLocation();
     const courseName = location.state?.name;
     const { id } = useParams();
+    const navigate = useNavigate();
 
     const deleteCourse = async (id: string) => {
         try {
             await axios.delete(`${API_BASE_URL}/courses/${id}`);
+            navigate("/") // navigate back to Home
         } catch(err) {
             console.error("Error deleting course:", err);
         }
@@ -20,8 +22,8 @@ export default function Course() {
     <>
         <h1 className="text-center text-4xl mt-1">API Study Tool</h1>
         <h3 className="text-center text-2xl">{courseName}</h3>
-        <button className='text-red-500 border-2 rounded-full p-2'
-        onClick={() => id && deleteCourse(id)}>Delete</button>
+        <button className='text-red-500 hover:text-red-700 border-2 rounded-full p-2'
+        onClick={() => id && deleteCourse(id)}>Delete Course</button>
     </>
   )
 }
