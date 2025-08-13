703f067c9d215225a156e335d603b4be00275ba3
jayrevel2004 <jaydenrevel@gmail.com>
2025-08-12 21:21:54 -0400

    Worked on FileUploader component. File uploads working through client

--- client/src/pages/Course.tsx
+++ client/src/pages/Course.tsx
@@ -1,12 +1,19 @@
 import { useLocation, useParams, useNavigate } from "react-router-dom"
+import { useEffect } from "react";
 import API_BASE_URL from "../assets/URL";
 import axios from "axios";
+import FileUploader from "../components/FileUploader";
 
 export default function Course() {
     const location = useLocation();
     const courseName = location.state?.name;
     const { id } = useParams();
     const navigate = useNavigate();
+
+    const loadResources = async () => {
+        const resources = await axios.get(`${API_BASE_URL}/files/${id}/resources`)
+        console.log(resources.data.resources);
+    }
 
     const deleteCourse = async (id: string) => {
         try {
@@ -16,6 +23,12 @@ export default function Course() {
             console.error("Error deleting course:", err);
         }
     }
+
+    if(!id) return <div>Course not found</div>;
+
+    useEffect(() => {
+        loadResources();
+    })
     
 
   return (
@@ -23,7 +36,10 @@ export default function Course() {
         <h1 className="text-center text-4xl mt-1">API Study Tool</h1>
         <h3 className="text-center text-2xl">{courseName}</h3>
         <button className='text-red-500 hover:text-red-700 border-2 rounded-full p-2'
-        onClick={() => id && deleteCourse(id)}>Delete Course</button>
+        onClick={() => id && deleteCourse(id)}>
+            Delete Course
+        </button>
+        <FileUploader courseId={id} />
     </>
   )
 }
